ac83b29389346ea209b74a6a1665ce92f5e9b021
Fabrice aka Farmer <Fabrice.Mpogazi@tufts.edu>
2020-05-04 19:32:29 -0400

    Item based collab. filtering

---
+++ src/math260/item_based_cf.py
@@ -0,0 +1,79 @@
+import data_prep
+import numpy as np
+import numpy.ma as ma
+
+
+'''
+Calculating the similarity between two items
+
+args: ratings1, ratings2 (arrays of ratings)
+eg: ratings1 = [1, 9.0, 1.0, 10.0, 8.7]
+
+returns: similarity_score (a float)
+'''
+def PearsonSimilarity(ratings1, ratings2):
+    X = np.array(ratings1['ratings'])
+    Y = np.array(ratings2['ratings'])
+    return ma.corrcoef(ma.masked_invalid(X), ma.masked_invalid(Y))[0][1]
+
+'''
+Creates a similarity matrix of the games
+For the moment, we are using just the PearsonSimilarity
+
+Optimizations: f(a, b) = f(b, a) so we might as well compute
+               similarities for half of the matrix
+               (However, are all the kernels gonna be symmetric??)
+
+args: games, review_matrix, f (similarity_metric)
+
+returns: similarity_matrix (as a np.array)
+'''
+def similarity_matrix(games_map, review_matrix, f=None):
+    matrix = []
+    games = games_map['forward']
+    for key in games:
+        for key2 in games:
+            id1, id2 = games[key], games[key2]
+            if (id1 == id2):
+                matrix.append(1)
+            else:
+                data = data_arrange([id1, id2], review_matrix)
+                matrix.append(PearsonSimilarity(data[0], data[1]))
+    return np.array(matrix, ndmin=2)
+
+
+'''
+Finds which users rated the two games and 
+returns two arrays (as a tuple) of those shared ratings
+    
+args: games_ids (array of ids of the two games), review_matrix
+    
+returns: (ratings1, ratings2)
+'''
+def data_arrange(games_ids, review_matrix):
+    rating_1 = {'id': games_ids[0], 'ratings':[]}
+    rating_2 = {'id': games_ids[1], 'ratings':[]}
+
+    review_dict_1 = {}
+    review_dict_2 = {}
+    for elem in review_matrix.toarray():
+        if (elem[2] == rating_1['id']):
+            rating_1['ratings'].append(elem)
+            review_dict_1[elem[1]] = elem
+        elif (elem[2] == rating_2['id']):
+            rating_2['ratings'].append(elem)
+            review_dict_2[elem[1]] = elem
+    
+    '''Computing the intersection of the two rating arrays'''
+    new_ratings_1 = []
+    new_ratings_2 = []
+    for elem in rating_2['ratings']:
+        if (elem[1] in review_dict_1):
+            new_ratings_2.append(elem[0])
+    for elem in rating_1['ratings']:
+        if (elem[1] in review_dict_2):
+            new_ratings_1.append(elem[0])
+    rating_1['ratings'] = new_ratings_1
+    rating_2['ratings'] = new_ratings_2
+    
+    return (rating_1, rating_2)
--- src/math260/main.py
+++ src/math260/main.py
@@ -1,4 +1,5 @@
 import data_prep
+import recommend
 
 GAMES_DATA_FILE = "data/2019-05-02-debug.csv"
 # This must be the debug dataset if working on a personal machine.
